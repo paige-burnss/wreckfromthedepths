a8102a97d6feb3fef07a7d7926db337812d6e427
Paige Burns <paige.burns@colorado.edu>
2023-02-17 14:33:24 +0100

    unfinished but added html and css for login and guest pages and started to organize dictionary - couldn't figure out how to get that to fully work yet so doing some hard coding but will make that work later

---
+++ control.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    $("#man").text(dict['man']);
+});
---
+++ dict.js
@@ -0,0 +1,57 @@
+function dict_get(item) {
+    return dict[lang][item];
+}
+
+let lang = "eng";
+
+
+function surpriseMe() {
+    lang = "surprise";
+    let ids = $('.translate').map(function () {
+        return this.id;
+    }).get().join().split(',')
+    for (id in ids) {
+        let item = ids[id];
+
+        $("#" + item).text(dict_get(item));
+    }
+    $("#surprise").hide(4000);
+}
+
+
+function switchLang() {
+    console.log(lang);
+
+    if (lang == "eng") {
+        lang = "swe";
+    } else if (lang == "swe") {
+        lang = "eng";
+    } else {
+        lang = "eng"
+    }
+
+    console.log(lang);
+
+    let ids = $('.translate').map(function () {
+        return this.id;
+    }).get().join().split(',')
+    for (id in ids) {
+        let item = ids[id];
+
+        $("#" + item).text(dict_get(item));
+    }
+}
+
+dict = {
+    "eng": {
+        "lang": "EN",
+        "guest": "Guest",
+        "vip": "VIP",
+        "man": "Manager",
+        "lg": "Logout of Guest",
+        "search": "Search"
+    },
+    "swe": {
+        "lang": "SV",
+    }
+};
