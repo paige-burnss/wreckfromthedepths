de21cf6d781c27580ac073458abeecf455c81ead
Sam Mowbray <sammowbray@MacBook-Pro-som-tillhor-Sam.local>
2023-02-27 14:06:05 +0100

    pushing changes

---
+++ createItem.js
@@ -0,0 +1,10 @@
+var itemContainer = document.getElementById("items-box");
+
+for (var i = 0; i < DB2.length; i++) {
+    var item = DB2[i];
+
+    var itemDiv =document.createElement("div");
+    itemDiv.classList.add("item");
+
+    var ite
+}
